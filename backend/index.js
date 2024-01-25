6461d5c296520b327620759bd4bbe92cd31dd073
Pawel Kmuk <kmuk.pawel@gmail.com>
2024-01-25 18:51:44 +0100

    change cors

--- backend/index.js
+++ backend/index.js
@@ -7,12 +7,7 @@ import cors from "cors"
 import multer from "multer"
 
 const app = express()
-// app.use(cors())
-app.use(cors({
-	origin: 'https://client-football.onrender.com',
-	methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-	credentials: true
-}));
+app.use(cors())
 app.use(express.json())
 
 
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { register, login } from "../controllers/auth.js";
+import cors from "cors"
 
 const router = express.Router()
 
 router.post('/register', register)
-router.post('/login', login)
+router.post('/login', cors(), login)
 router.post('/logout', ()=>{})
 
 export default router
