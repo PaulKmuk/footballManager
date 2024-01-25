bcd30900b29ba25514f646131175bea5141bfe35
Pawel Kmuk <kmuk.pawel@gmail.com>
2024-01-25 10:41:13 +0100

    change connection db

--- backend/db.js
+++ backend/db.js
@@ -1,9 +1,16 @@
 import mysql from "mysql"
 
 
+// export const db = mysql.createConnection({
+//     host: "localhost",
+//     user: "root",
+//     password: "root",
+//     database: "football"
+// })
+
 export const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "root",
-    database: "football"
+    host: "sql8.freesqldatabase.com",
+    user: "sql8679656",
+    password: "3nek3McG6M",
+    database: "sql8679656"
 })
